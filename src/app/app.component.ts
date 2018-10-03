c1e1fb7b8a52f57a9fa3fc5a57639e0799e53a83
Maina Wycliffe <wmmaina7@gmail.com>
2018-10-03 14:30:28 +0300

    added drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-material7-drag-and-drop-demo';
+
+  public todo: Todo[] = [
+    { title: 'Get to work', dateAdded: new Date().toString() },
+    { title: 'Pick up groceries', dateAdded: new Date().toString() },
+    { title: 'Go home', dateAdded: new Date().toString() },
+    { title: 'Fall asleep', dateAdded: new Date().toString() }
+  ];
+
+  public done: Todo[] = [
+    { title: 'Get up', dateAdded: new Date().toString() },
+    { title: 'Brush teeth', dateAdded: new Date().toString() },
+    { title: 'Take a shower', dateAdded: new Date().toString() },
+    { title: 'Check e-mail', dateAdded: new Date().toString() },
+    { title: 'Walk dog', dateAdded: new Date().toString() }
+  ];
+
+  public cancelled: Todo[] = [];
+
+  drop(event: CdkDragDrop<Todo[]>) {
+    console.log(event);
+
+    // first check if it was moved within the same list or moved to a different list
+    if (event.previousContainer === event.container) {
+      // change the items index if it was moved within the same list
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      // remove item from the previous list and add it to the new array
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addItem(list: string, todo: string) {
+    if (list === 'todo') {
+      this.todo.push({ title: todo, dateAdded: new Date().toString() });
+    } else {
+      this.done.push({ title: todo, dateAdded: new Date().toString() });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
